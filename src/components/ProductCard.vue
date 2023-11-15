ada79830ba00f9d959569e47148bda32846e9ab4
Apisit B <virus_eddy@hotmail.com>
2023-11-15 23:48:37 +0700

    Update everythinbg

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -10,25 +10,45 @@ defineProps({
 </script>
 
 <template>
+  <div class="product-container">
+
   <!-- <RouterLink to="book/100001"> -->
   <RouterLink :to="{ name: 'book-details', params: {id: book.id} }" >
     <div class="product-card">
+      <img :src="book.image" alt="Book Image" />
       <h2>{{ book.title }}</h2>
-      <span>Price: {{ book.price }} B , Condition: {{ book.condition }}</span>
     </div>
+  
   </RouterLink>
+</div>
 </template>
 
 <style scoped>
+.product-container {
+  display: flex;
+  flex-wrap: wrap; /* Allow items to wrap to the next line */
+  justify-content: center; /* Center items horizontally */
+  gap: 10px; /* Space between the cards */
+}
 .product-card {
-  padding: 20px;
-  width: 250px;
+  padding: 10px; /* Reduced padding */
+  width: 200px; /* Set a specific width */
+  max-height: 300px; /* Set a maximum height */
+  box-sizing: border-box; /* Include padding and border in the width and height */
   cursor: pointer;
   border: 1px solid #39495c;
   margin-bottom: 18px;
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* Center content horizontally */
 }
+
 .product-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+.product-card img {
+  max-width: 100px; 
+  max-height: 100px; 
 }
 </style>
--- src/views/BookDetailsView.vue
+++ src/views/BookDetailsView.vue
@@ -3,14 +3,12 @@ import { ref, onMounted } from 'vue'
 import BookService from '@/services/BookService.js'
 
 const theBook = ref(null)
-// const id = ref(100001)
 
 const props = defineProps({
     id: { required: true }
 })
 
 onMounted(() => {
-    //BookService.getBook(id.value)
     BookService.getBook(props.id)
     .then((response)=>{
         theBook.value = response.data
@@ -24,8 +22,7 @@ onMounted(() => {
 <template>
     <div v-if="theBook">
         <h1>{{  theBook.title }}</h1>
-        <p>Price: {{ theBook.price }} Condition: {{ theBook.condition }} </p>
-        <p>ISBN: {{ theBook.isbn }} Published on: {{ theBook.published }}</p>
-        <p>Free Shipping: {{ theBook.isbn }}</p>
+        <p>Type: {{ theBook.price }} Abilities: {{ theBook.condition }} </p>
+        <p>Catch rate: {{ theBook.isbn }} Hatch time: {{ theBook.published }}</p>
     </div>
 </template>
